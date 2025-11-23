e9cdafc6736a5e516b185d42cbb03c5f93f5cb4b
Nicu <domenti.nicolae@proton.me>
2025-11-23 03:18:32 +0200

    Made the connection between the back-end and the front-end v2

---
+++ frontend/app/Providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import ThemeProvider from "./ThemeProvider";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position="bottom-center" reverseOrder={false} />
+        {children}
+      </QueryClientProvider>
+    </ThemeProvider>
+  );
+}
+
